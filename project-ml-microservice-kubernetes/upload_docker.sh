866f47b02028d3201bc3c8c2c4d7f6b2b218a947
mohamedsaad308 <mohamedsaad308@gmail.com>
2022-08-08 11:57:07 +0000

    update upload docker script

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=mohamedsaad308/ml-microservice
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +13,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push mohamedsaad308/ml-microservice:v1.0.0
